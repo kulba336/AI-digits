bf7427d47fe82cc56820276bd8e799e7617d542e
kulba336 <fistashkamorojenoe@gmail.com>
2026-03-12 15:47:43 +0800

    digits visualization

--- main.py
+++ main.py
@@ -20,4 +20,16 @@ print(f'Data test shape: {data_test.shape}\n'
 unique, count = np.unique(target_train, return_counts=True)
 for digit, count in zip(unique,count):
     print(f'Digit: {digit}, count: {count} - {count/len(target_train)*100:.1f}%')
-    
+
+# График - примеры цифр
+fig, ax = plt.subplots(4,10,figsize = (15,6))
+
+for i in range(40):
+    axes = ax[i//10, i%10] # разбиаение диапозона range(40) на двумерный массив [row, col]
+    axes.imshow(data_train[i], cmap = 'gray') # отрисовка картинки с цифрой
+    axes.set_title(f'{target_train[i]}', fontsize = 12)
+    axes.axis('off') # отключение отображения оси координат для графика
+
+plt.suptitle('Digits example', fontsize = 16, fontweight = 'bold')
+plt.tight_layout()
+plt.savefig('1_digits_example.png', dpi = 150, bbox_inches = 'tight')
