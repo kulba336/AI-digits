f760f9f2fbf33391c2a58c7eec286b8f4c6992e4
kulba336 <fistashkamorojenoe@gmail.com>
2026-03-12 15:22:10 +0800

    first

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# init
+from keras.datasets import mnist
+import matplotlib.pyplot as plt 
+
+# === 1. Подгрузка датасета ===
+(data_train, target_train), (data_test, target_test) = mnist.load_data()
+
+# формат данных
+print(f'Data tain shape: {data_train}\n'
+      f'Target train shape: {target_train.shape}\n'
+      f'Dtype: {data_train.dtype}\n'
+      f'Value range: {data_train.min()} - {data_train.max()}\n')
+
+print(f'Data test shape: {data_test.shape}\n'
+      f'Target test shape: {target_test.shape}\n'
+      f'Dtype: {target_train.dtype}\n'
+      f'Value range: {target_train.min()} - {target_train.max()}')
