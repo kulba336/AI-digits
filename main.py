226dca18e1555539bd6e15f4a61c208f192b4dca
kulba336 <fistashkamorojenoe@gmail.com>
2026-03-12 15:32:06 +0800

    count unique digits

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from keras.datasets import mnist
 import matplotlib.pyplot as plt 
+import numpy as np
 
 # === 1. Подгрузка датасета ===
 (data_train, target_train), (data_test, target_test) = mnist.load_data()
@@ -14,3 +15,9 @@ print(f'Data test shape: {data_test.shape}\n'
       f'Target test shape: {target_test.shape}\n'
       f'Dtype: {target_train.dtype}\n'
       f'Value range: {target_train.min()} - {target_train.max()}')
+
+# Подсчёт уникальных классов (целевых данных)
+unique, count = np.unique(target_train, return_counts=True)
+for digit, count in zip(unique,count):
+    print(f'Digit: {digit}, count: {count} - {count/len(target_train)*100:.1f}%')
+    
