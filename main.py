e847c1be36eeddd187df7de2604f7e8fd319aa54
kulba336 <fistashkamorojenoe@gmail.com>
2026-03-17 15:49:37 +0800

    data reshape

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from keras.datasets import mnist
+from tensorflow.keras.datasets import mnist
 import matplotlib.pyplot as plt 
 import numpy as np
 
@@ -48,3 +48,17 @@ data_test_norm = data_test.astype('float32')/255.0
 
 print(f'Before norm: [{data_train.min()} - {data_train.max()}]\n'
       f'After norm: [{data_train_norm.min():.2f} - {data_train_norm.max():.2f}]\n')
+
+# reshape формы для слоя Dense
+data_train_flat = data_train_norm.reshape(-1,28*28)
+data_test_flat = data_test_norm.reshape(-1,28*28)
+
+print(f'Before reshape:\n'
+      f'data_train_norm: {data_test_norm.shape}\n'
+      f'single digit: {data_test_norm[0].shape}\n')
+
+print(f'After reshape:\n'
+      f'data_train_flat: {data_train_flat.shape}\n'
+      f'single digit: {data_train_flat[0].shape}\n')
+
+print(f'Check: {28} * {28} = {28*28}')
